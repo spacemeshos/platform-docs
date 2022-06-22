3eb60765240fff5cd8aff11f3f4d76e807914951
yaelmhoffman <56118594+yaelmhoffman@users.noreply.github.com>
2022-06-22 09:48:18 +0300

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,16 +1,4 @@
 // sidebar for next version
 module.exports = {
-  someSidebar: {
-        'Platform' : ['platform'],
-        'Protocol': [
-            'protocol/overview',
-            'protocol/p2p/overview',
-            {'Consensus': ['protocol/consensus/overview', 'protocol/consensus/deepdive']},
-            {'Hare': ['protocol/hare/overview', 'protocol/hare/faq']},
-            {'Mining': ['protocol/mining/overview', 'protocol/mining/post', 'protocol/mining/poet', 'protocol/mining/activations']},
-            'protocol/sync/overview',
-            'protocol/transactions/overview'
-        ],
-        'API': ['api/overview']
-    }
+  someSidebar: {}
 };
